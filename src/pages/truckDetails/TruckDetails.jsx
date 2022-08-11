d42a1fc6e830968c2e71fc21b1ff418a2b3f3148
Jordan <jwald3@brockport.edu>
2022-08-11 13:07:18 -0400

    added single truck and facility tablet views

--- src/pages/truckDetails/TruckDetails.jsx
+++ src/pages/truckDetails/TruckDetails.jsx
@@ -94,6 +94,77 @@ const TruckDetails = ({ details }) => {
             </div>
         </div>
     )
+    // return (
+    //     <div className='truckDetails'>
+    //         <Sidebar />
+    //         <div className="singleContainer">
+    //             <Navbar />
+    //             <div className="top">
+    //                 <div className="left">
+    //                     <div className="detail-card">
+    //                         <div className="bio">
+    //                             <div className="resourceImg">
+    //                                 <img src={'/semi-truck.png'} alt="" className='itemImg'/>
+    //                             </div>      
+    //                             <div className="editBtn">
+    //                                 <span>Edit</span><EditIcon className='icon'/>
+    //                             </div>
+    //                         </div>
+    //                         <hr />
+    //                         <div className="info">
+    //                             {
+    //                                 details.map(detail => (
+    //                                     <div className="detailItem" key={detail.id}>
+    //                                         <span className="itemKey">{detail.label}</span>
+    //                                         <span className="itemValue">{data[detail.field]}</span>
+    //                                     </div>
+    //                                 ))
+    //                             }
+    //                         </div>
+                            
+    //                     </div>
+    //                 </div>
+    //                 <div className="left">
+    //                     <div className="detail-card">
+    //                         <div className="bio">
+    //                             <div className="resourceImg">
+    //                                 <img src={'/semi-truck.png'} alt="" className='itemImg'/>
+    //                             </div>      
+    //                             <div className="editBtn">
+    //                                 <span>Edit</span><EditIcon className='icon'/>
+    //                             </div>
+    //                         </div>
+    //                         <hr />
+    //                         <div className="info">
+    //                             {
+    //                                 details.map(detail => (
+    //                                     <div className="detailItem" key={detail.id}>
+    //                                         <span className="itemKey">{detail.label}</span>
+    //                                         <span className="itemValue">{data[detail.field]}</span>
+    //                                     </div>
+    //                                 ))
+    //                             }
+    //                         </div>
+                            
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //             <div className="bottom">
+    //                 <h1 className="title">Last Trips</h1>
+    //                 <DataGrid
+    //                     rows={trips}
+    //                     columns={fields}
+    //                     autoHeight={true}
+    //                     pageSize={10}
+    //                     rowsPerPageOptions={[10]}
+    //                     checkboxSelection
+    //                     className='datagrid'
+    //                     getRowHeight={() => 'auto'}
+    //                 />
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
 }
 
 export default TruckDetails
